0d826e76a04d39a089e5ccfe0d32d0045939f413
Chuck Tilbury <chucktilbury@gmail.com>
2020-05-08 17:28:56 -0500

    initial commit

---
+++ build/src/CMakeFiles/simpler.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/chuck/Src/simpler/src/cmdline.c" "/home/chuck/Src/simpler/build/src/CMakeFiles/simpler.dir/cmdline.c.o"
+  "/home/chuck/Src/simpler/src/errors.c" "/home/chuck/Src/simpler/build/src/CMakeFiles/simpler.dir/errors.c.o"
+  "/home/chuck/Src/simpler/src/hash_table.c" "/home/chuck/Src/simpler/build/src/CMakeFiles/simpler.dir/hash_table.c.o"
+  "/home/chuck/Src/simpler/src/lists.c" "/home/chuck/Src/simpler/build/src/CMakeFiles/simpler.dir/lists.c.o"
+  "/home/chuck/Src/simpler/src/parser.c" "/home/chuck/Src/simpler/build/src/CMakeFiles/simpler.dir/parser.c.o"
+  "/home/chuck/Src/simpler/src/scanner.c" "/home/chuck/Src/simpler/build/src/CMakeFiles/simpler.dir/scanner.c.o"
+  "/home/chuck/Src/simpler/src/simpler.c" "/home/chuck/Src/simpler/build/src/CMakeFiles/simpler.dir/simpler.c.o"
+  "/home/chuck/Src/simpler/src/stacks.c" "/home/chuck/Src/simpler/build/src/CMakeFiles/simpler.dir/stacks.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/simpler.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "../../src/cmdline.c"
+  "../../src/scanner.c"
+  "CMakeFiles/simpler.dir/errors.c.o"
+  "CMakeFiles/simpler.dir/hash_table.c.o"
+  "CMakeFiles/simpler.dir/lists.c.o"
+  "CMakeFiles/simpler.dir/parser.c.o"
+  "CMakeFiles/simpler.dir/simpler.c.o"
+  "CMakeFiles/simpler.dir/stacks.c.o"
+  "CMakeFiles/simpler.dir/cmdline.c.o"
+  "CMakeFiles/simpler.dir/scanner.c.o"
+  "../../bin/simpler.pdb"
+  "../../bin/simpler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simpler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
